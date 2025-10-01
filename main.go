30a2a34e20f38ec550a27ee56b04396efc85e15f
Kyle Martin <kbm-ky@outlook.com>
2025-10-01 17:56:52 -0500

    Actual database integration

--- main.go
+++ main.go
@@ -1,11 +1,17 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"fmt"
 	"log"
 	"os"
+	"time"
 
+	"github.com/google/uuid"
 	"github.com/kbm-ky/gator/internal/config"
+	"github.com/kbm-ky/gator/internal/database"
+	_ "github.com/lib/pq"
 )
 
 func main() {
@@ -15,12 +21,21 @@ func main() {
 		log.Fatalf("unable to read config: %v", err)
 	}
 
+	//Prepare database
+	db, err := sql.Open("postgres", configFile.DbUrl)
+	if err != nil {
+		log.Fatalf("unable to open databse: %s", configFile.DbUrl)
+	}
+
+	dbQueries := database.New(db)
+
 	// Prepare sub commands
-	s := state{config: &configFile}
+	s := state{db: dbQueries, cfg: &configFile}
 	cmds := commands{
 		handlers: map[string]func(*state, command) error{},
 	}
 	cmds.register("login", handlerLogin)
+	cmds.register("register", handlerRegister)
 
 	//finally check command line and dispatch
 	if len(os.Args) < 2 {
@@ -40,7 +55,8 @@ func main() {
 }
 
 type state struct {
-	config *config.Config
+	db  *database.Queries
+	cfg *config.Config
 }
 
 type command struct {
@@ -72,11 +88,44 @@ func handlerLogin(s *state, cmd command) error {
 	}
 
 	userName := cmd.args[0]
-	if err := s.config.SetUser(userName); err != nil {
+	_, err := s.db.GetUser(context.Background(), userName)
+	if err != nil {
+		log.Printf("user does not exist! %s\n", userName)
+		os.Exit(1)
+	}
+
+	if err := s.cfg.SetUser(userName); err != nil {
 		return fmt.Errorf("unable to set user: %w", err)
 	}
 
 	fmt.Printf("user has been set to '%s'\n", userName)
+
+	return nil
+}
+
+func handlerRegister(s *state, cmd command) error {
+	//Check args
+	if len(cmd.args) != 1 {
+		return fmt.Errorf("register command expects 1 argument: name")
+	}
+	name := cmd.args[0]
+
+	now := time.Now()
+	userParams := database.CreateUserParams{
+		ID:        uuid.New(),
+		CreatedAt: now,
+		UpdatedAt: now,
+		Name:      name,
+	}
+	user, err := s.db.CreateUser(context.Background(), userParams)
+	if err != nil {
+		log.Printf("name already exists! %s", name)
+		os.Exit(1)
+	}
+
+	s.cfg.SetUser(name)
+	fmt.Printf("user '%s' created\n", name)
+	fmt.Printf("user = %v\n", user)
 
 	return nil
 }
